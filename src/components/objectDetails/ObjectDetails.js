c62e360478297237c75b4f28a60687dd1ea6e605
Ruben L <dummy@email.address>
2020-07-02 19:32:40 +0200

    makes component css modular

--- src/components/objectDetails/ObjectDetails.js
+++ src/components/objectDetails/ObjectDetails.js
@@ -1,29 +1,59 @@
 import React from 'react'
 //import { Format } from '../components/format/Format'
-import './ObjectDetails.css'
+import css from './ObjectDetails.module.css'
 
 
 export function ObjectDetails(props) {
   const {
-    objectData = null
+    objectData = null,
+    onClose,
   } = props
 
   return (
-    <div className="ObjectDetails">
-      <header className="box">
-        header {!objectData && '(no data)'}
+    <div className={'ObjectDetails ' + css.root}>
+      <header className={css.box}>
+        <div className="title">
+          header {!objectData && '(no data)'}
+        </div>
+
+        <div className="time -info">
+          <i>clock</i>
+          2020-20-20
+          <span className="-remaining">
+            Noch x Wochen
+          </span>
+        </div>
+        <div className="time -meter"></div>
+
+        <button className="close"
+          onClick={onClose}
+        >
+          <i>X</i>
+        </button>
       </header>
 
-      <div className="box -a-1">a1</div>
-      <div className="box -a-2">a2</div>
-      <div className="box -a-3">a3</div>
-      <div className="box -a-4">a4</div>
+      <div className={`${css.box} ${css.a1}`}>
+        a1
+      </div>
+      <div className={`${css.box} ${css.a2}`}>
+        a2
+      </div>
+      <div className={`${css.box} ${css.a3}`}>
+        a3
+      </div>
+      <div className={`${css.box} ${css.a4}`}>
+        a4
+      </div>
 
-      <div className="box -diagram">D</div>
+      <div className={`${css.box} ${css.diagram}`}>
+        D
+      </div>
 
-      <div className="box -customers">C</div>
+      <div className={`${css.box} ${css.customers}`}>
+        C
+      </div>
 
-      <footer className="box">footer </footer>
+      <footer className={css.box}>footer </footer>
     </div>
   )
 }
