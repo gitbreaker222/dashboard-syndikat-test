7106a3bb1a6af701efd335bf33963fe8145f7c6b
Ruben L <dummy@email.address>
2020-07-03 23:18:53 +0200

    fixes circle diagram with value < 50%

--- src/components/circleDiagram/CircleDiagram.js
+++ src/components/circleDiagram/CircleDiagram.js
@@ -12,23 +12,20 @@ export function CircleDiagram(props) {
   const c2_r = 42; // ;)
   const c3_r = 34;
 
-  const c1_strokeLength = 2 * c1_r * Math.PI
-  const c2_strokeLength = 2 * c2_r * Math.PI
-  const c3_strokeLength = 2 * c3_r * Math.PI
-
   const calcLength = (fullLength, percent) => {
     return fullLength * percent / 100
   }
 
-  const style1 = {
-    strokeDasharray: calcLength(c1_strokeLength, c1Percent) + 'px'
+  const getStyle = (radius, percent) => {
+    const strokeLength = 2 * radius * Math.PI
+    return {
+      strokeDasharray: calcLength(strokeLength, percent) + 'px ' + strokeLength
+    }
   }
-  const style2 = {
-    strokeDasharray: calcLength(c2_strokeLength, c2Percent) + 'px'
-  }
-  const style3 = {
-    strokeDasharray: calcLength(c3_strokeLength, c3Percent) + 'px'
-  }
+
+  const style1 = getStyle(c1_r, c1Percent)
+  const style2 = getStyle(c2_r, c2Percent)
+  const style3 = getStyle(c3_r, c3Percent)
 
   return (
     <svg viewBox="0 0 110 110"
